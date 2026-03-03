8b30a60c0b53967d15cc050d404a8600e121dec1
Pierre Gronnier <pierre.gronnier@gmail.com>
2026-03-03 15:37:40 +0100

    feat: init both repos nidus-client and nidus-server

---
+++ nidus-server/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(helmet());
+
+// CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true, // indispensable pour les cookies
+  }),
+);
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+
+export default app;
