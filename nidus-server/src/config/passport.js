3a7b8eda1c143f13976566cac215102513746890
Pierre Gronnier <pierre.gronnier@gmail.com>
2026-04-15 16:40:33 +0200

    fix: link existing user by email on Google OAuth to avoid unique constraint error

--- nidus-server/src/config/passport.js
+++ nidus-server/src/config/passport.js
@@ -1,7 +1,10 @@
 import passport from "passport";
 import prisma from "../config/prisma.js";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { findUserByGoogleId } from "../services/user.service.js";
+import {
+  findUserByGoogleId,
+  findUserByEmail,
+} from "../services/user.service.js";
 
 passport.use(
   new GoogleStrategy(
@@ -12,20 +15,29 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        const user = await findUserByGoogleId(profile.id);
-        if (!user) {
-          const newUser = await prisma.user.create({
-            data: {
-              email: profile.emails[0].value,
-              username: profile.displayName,
-              avatarUrl: profile.photos[0].value,
-              googleId: profile.id,
-            },
+        let user = await findUserByGoogleId(profile.id);
+        if (user) return done(null, user);
+
+        const email = profile.emails[0].value;
+
+        user = await findUserByEmail(email);
+        if (user) {
+          user = await prisma.user.update({
+            where: { id: user.id },
+            data: { googleId: profile.id },
           });
-          return done(null, newUser);
-        } else {
           return done(null, user);
         }
+
+        const newUser = await prisma.user.create({
+          data: {
+            email,
+            username: profile.displayName,
+            avatarUrl: profile.photos[0].value,
+            googleId: profile.id,
+          },
+        });
+        return done(null, newUser);
       } catch (error) {
         done(error);
       }
